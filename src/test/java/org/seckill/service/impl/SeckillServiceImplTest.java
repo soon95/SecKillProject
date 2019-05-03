2823d937a01cbd1e09e9eb3b1cad816980ef0f18
Leon Song <songliu0608@qq.com>
2019-05-03 22:03:45 +0800

    web 开发

--- src/test/java/org/seckill/service/impl/SeckillServiceImplTest.java
+++ src/test/java/org/seckill/service/impl/SeckillServiceImplTest.java
@@ -19,74 +19,76 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({"classpath:spring/spring-dao.xml",
-					   "classpath:spring/spring-service.xml"})
+@ContextConfiguration({ "classpath:spring/spring-dao.xml", "classpath:spring/spring-service.xml" })
 public class SeckillServiceImplTest {
-	private final Logger logger=LoggerFactory.getLogger(this.getClass());
-	
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
 	@Autowired
 	private SeckillService SeckillService;
-	
+
 	@Test
 	public void testGetSeckillList() {
-		List<Seckill> list=SeckillService.getSeckillList();
-		logger.info("list={}",list);
+		List<Seckill> list = SeckillService.getSeckillList();
+		logger.info("list={}", list);
 	}
 
 	@Test
 	public void testGetById() {
-		long seckillId=1005;
-		Seckill seckill=SeckillService.getById(seckillId);
-		logger.info("seckill={}",seckill);
+		long seckillId = 1005;
+		Seckill seckill = SeckillService.getById(seckillId);
+		logger.info("seckill={}", seckill);
 	}
 	/*
-	 * seckill=Seckill [seckillId=1005, name=800元秒杀ipad, number=200, startTime=Tue Jan 01 08:00:00 CST 2019, endTime=Tue Oct 01 08:00:00 CST 2019, createTime=Thu May 02 23:48:51 CST 2019]
+	 * seckill=Seckill [seckillId=1005, name=800元秒杀ipad, number=200, startTime=Tue
+	 * Jan 01 08:00:00 CST 2019, endTime=Tue Oct 01 08:00:00 CST 2019,
+	 * createTime=Thu May 02 23:48:51 CST 2019]
 	 */
 
 	@Test
 	public void testExportSeckillUrl() {
-		long id=1004;
-		Exposer exposer=SeckillService.exportSeckillUrl(id);
-		logger.info("exposer={}",exposer);
+		long id = 1004;
+		Exposer exposer = SeckillService.exportSeckillUrl(id);
+		logger.info("exposer={}", exposer);
 	}
 	/*
-	 * exposer=Exposer [exposed=true, md5=62fccdcec7ee55af1fabf6145e12d687, seckillId=1005, now=0, start=0, end=0]
-	 * exposer=Exposer [exposed=true, md5=b85b6f1df88db4422019a9c97164b01d, seckillId=1004, now=0, start=0, end=0]
+	 * exposer=Exposer [exposed=true, md5=62fccdcec7ee55af1fabf6145e12d687,
+	 * seckillId=1005, now=0, start=0, end=0] exposer=Exposer [exposed=true,
+	 * md5=b85b6f1df88db4422019a9c97164b01d, seckillId=1004, now=0, start=0, end=0]
 	 */
 
 	@Test
 	public void testExecuteSeckill() {
-		long id=1004;
-		long phone=15850164578L;
-		String md5="b85b6f1df88db4422019a9c97164b01d";
+		long id = 1004;
+		long phone = 15850164578L;
+		String md5 = "b85b6f1df88db4422019a9c97164b01d";
 		try {
-			SeckillExecution execution=SeckillService.executeSeckill(id, phone, md5);
-			logger.info("execution={}",execution);
-		} catch (RepeatKillException|SeckillCloseException e) {
+			SeckillExecution execution = SeckillService.executeSeckill(id, phone, md5);
+			logger.info("execution={}", execution);
+		} catch (RepeatKillException | SeckillCloseException e) {
 			logger.error(e.getMessage());
 		}
 	}
-	
+
 	@Test
 	public void testSeckillLogic() {
-		long id=1007;
-		Exposer exposer=SeckillService.exportSeckillUrl(id);
+		long id = 1007;
+		Exposer exposer = SeckillService.exportSeckillUrl(id);
 		if (exposer.isExposed()) {
-			logger.info("exposer={}",exposer);
-			long phone=15850164578L;
-			String md5=exposer.getMd5();
-			
+			logger.info("exposer={}", exposer);
+			long phone = 15850164578L;
+			String md5 = exposer.getMd5();
+
 			try {
-				SeckillExecution execution=SeckillService.executeSeckill(id, phone, md5);
-				logger.info("execution={}",execution);
-			} catch (RepeatKillException|SeckillCloseException e) {
+				SeckillExecution execution = SeckillService.executeSeckill(id, phone, md5);
+				logger.info("execution={}", execution);
+			} catch (RepeatKillException | SeckillCloseException e) {
 				logger.error(e.getMessage());
 			}
 		} else {
-			//秒杀未开启
-			logger.warn("exposer={}",exposer);
+			// 秒杀未开启
+			logger.warn("exposer={}", exposer);
 		}
-		
+
 	}
 
 }
